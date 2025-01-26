72f649f294bddc10f12dec47779acf0c33796dc3
Joshi Sarthak Prasad <113760347+joshisarthak15@users.noreply.github.com>
2025-01-26 17:21:42 +0000

    new files added

---
+++ 01_basics/conversion.js
@@ -0,0 +1,39 @@
+let age = true
+
+console.log(typeof age)
+console.log(typeof (age))
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN(Not A Number)
+// true =>1 , false =>0
+
+let valuebool = "Sarthak"
+
+let booleanvalueof = Boolean(valuebool)
+console.log(typeof booleanvalueof)
+console.log(booleanvalueof);
+
+
+// 1 =>true , 0 =>false
+// "" =>false
+// "Sarthak" =>true
+
+let numberof = 15
+
+let stringNumber = String(numberof)
+
+console.log(typeof stringNumber)
+console.log(stringNumber);
+
+/*****************Operation*******************/
+
+console.log(1+"2"); //output will be 12
+console.log("1"+2); //output will be 12
+console.log("1"+2+2);//output will be 122
+console.log(1+2+"2");//outpput will be 32
+
+
